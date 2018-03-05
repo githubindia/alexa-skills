1efaf5b0adf6af1245a222c23fcf40a56d83749a
AbhishekDash-Hexaware <shubham08gupta08@gmail.com>
2018-03-05 14:20:46 +0530

    handlers added

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var app = express();
 // Initialize the Alexa SDK
 var Alexa = require('alexa-sdk');
 app.use(bodyParser.json());
+
 app.post('/', function(req, res) {
     console.log("request received");
     // Build the context manually, because Amazon Lambda is missing
@@ -16,28 +17,38 @@ app.post('/', function(req, res) {
             console.log(error);
         }
     };
+    
     // Delegate the request to the Alexa SDK and the declared intent-handlers
     var alexa = Alexa.handler(req.body, context);
+    alexa.appId = "amzn1.ask.skill.f90712ef-cf51-4276-b1b6-bbcac167b830";
     alexa.registerHandlers(handlers);
     alexa.execute();
+});
+
+app.get('/', function(req, res) {
+    console.log("inside get");
 });
 
 var handlers = {
     'LaunchRequest': function() {
 
-        this.emit('HelloWorldIntent');
+        this.emit('WheatherIntent');
     },
-    'HelloWorldIntent': function() {
+    'WeatherIntent': function() {
 
         var message = "Hello from node JS";
 
         this.response.speak(message);
         this.emit(':responseReady');
+
     },
     'SessionEndedRequest': function() {
 
         console.log('session ended!');
         this.attributes['endedSessionCount'] += 1;
+    },
+    'Unhandled': function () {
+        this.emit(':ask', HelpMessage, HelpMessage);
     }
 };
 
