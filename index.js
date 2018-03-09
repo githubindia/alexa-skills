33fb3d51877d9f1ebe38fe9678f5c9f3e5eb981a
AbhishekDash-Hexaware <shubham08gupta08@gmail.com>
2018-03-09 12:43:08 +0530

    response changed

--- index.js
+++ index.js
@@ -137,23 +137,37 @@ app.post('/webhook', function(req, res){
             };
             request(options, function(err, response, body){
                 console.log("inside request");
-                //console.log(JSON.stringify(response) + "response");
-                //outputSpeechText = "humidity is " + response.main.humidity + "with " + response.weather.description + ".";
-            });
-            if (this.event.request.dialogState == "STARTED") {
-                console.log("inside delegate");
-            responseBody = {
-                "version": "1.0",
-                "response": {
-                "directives": [
-                        {
-                            "type": "Dialog.Delegate"
+                console.log(JSON.stringify(response) + "response");
+                outputSpeechText = "humidity is " + response.main.humidity + "with " + response.weather.description + ".";
+                if (this.event.request.dialogState == "STARTED") {
+                    responseBody = {
+                        "version": "1.0",
+                        "response": {
+                        "outputSpeech": {
+                            "type": "PlainText",
+                            "text": outputSpeechText
+                        },
+                        "card": {
+                            "type": "Simple",
+                            "title": "cityIntent",
+                            "content": "Hello from JS."
+                        },
+                        "reprompt": {
+                            "outputSpeech": {
+                            "type": "PlainText",
+                            "text": "Say a command"
+                            }
+                        },
+                        "directives": [
+                                {
+                                    "type": "Dialog.Delegate"
+                                }
+                        ],
+                        "shouldEndSession": false
                         }
-                ],
-                "shouldEndSession": false
+                    };
                 }
-            };
-            }
+            });
       }
       } else {
       // Not a recognized type
