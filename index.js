71174f94d81a4a3ea9043cd51b74a490f0f2dfd0
AbhishekDash-Hexaware <shubham08gupta08@gmail.com>
2018-03-12 11:30:12 +0530

    indendation corrected

--- index.js
+++ index.js
@@ -78,163 +78,160 @@ app.post('/webhook', function(req, res){
   });
 
   // Called when all data has been accumulated
-  req.on('end', function(){
-    var responseBody = {};
+    req.on('end', function()  {
+        var responseBody = {};
 
-    // parsing the requestBody for information
-    var jsonData = JSON.parse(requestBody);
-    if(jsonData.request.type == "LaunchRequest")
-    {
-      // crafting a response
-      responseBody = {
-        "version": "0.1",
-        "response": {
-          "outputSpeech": {
-            "type": "PlainText",
-            "text": "Welcome to Shubham's Stormy weather assistant! Please say a command."
-          },
-          "card": {
-            "type": "Simple",
-            "title": "Opened",
-            "content": "You started the Node.js Echo API Sample"
-          },
-          "reprompt": {
-            "outputSpeech": {
-              "type": "PlainText",
-              "text": "Say a command"
-            }
-          },
-          "shouldEndSession": false
-        }
-      };
-    } else if(jsonData.request.type == "IntentRequest")
-    {
-      var outputSpeechText = "";
-      var cardContent = "";
-      if (jsonData.request.intent.name == "WeatherIntent")
-      {
-        //response modified
-        // The Intent "TurnOn" was successfully called
-        responseBody = {
-        "version": '1.0',
-        "response": {
-            "shouldEndSession": true,
-            "outputSpeech": { "type": 'SSML', "ssml": '<speak> Hello from node JS </speak>' } 
-        },
-        "sessionAttributes": {},
-        "userAgent": 'ask-nodejs/1.0.25 Node/v6.10.0'
-    }
-    } else if (jsonData.request.intent.name == "cityIntent") {
-          if (typeof jsonData.request.intent.slots.cityName.value != "undefined") {
-            var city = jsonData.request.intent.slots.cityName.value;
-            if (jsonData.request.dialogState == "STARTED") {
-                //var outputSpeechText = "humidity is " + response.body.main.humidity + " with " + response.body.weather[0].description + ".";
-                responseBody = {
-                  "version": "1.0",
-                  "response": {
-                  "directives": [
-                    {
-                      "type": "Dialog.Delegate",
-                      "updatedIntent": {
-                        "name": "cityIntent",
-                        "confirmationStatus": "NONE",
-                        "slots": {
-                          "cityName": {
-                            "name": "cityName",
-                            "value": city,
-                            "confirmationStatus": "NONE"
-                          }
-                        }
-                      }
-                    }
-                  ],
-                  "shouldEndSession": false
-                  }
-              };
-            } else if (jsonData.request.dialogState == "IN_PROGRESS" && jsonData.request.intent.slots.cityName.confirmationStatus == "CONFIRMED") {
-              let response = deasync(function(callback){
-                weather.cityWeather(city, callback);
-              })();
-
-              var outputSpeechText = "humidity is " + response.body.main.humidity + " with " + response.body.weather[0].description + ".";
-              responseBody = {
-                "version": '1.0',
+        // parsing the requestBody for information
+        var jsonData = JSON.parse(requestBody);
+        if(jsonData.request.type == "LaunchRequest") {
+        // crafting a response
+            responseBody = {
+                "version": "0.1",
                 "response": {
-                    "shouldEndSession": true,
-                    "outputSpeech": { "type": 'SSML', "ssml": '<speak>' + outputSpeechText + '</speak>' } 
-                },
-                "sessionAttributes": {},
-                "userAgent": 'ask-nodejs/1.0.25 Node/v6.10.0'
-            }
-            } else if (jsonData.request.dialogState == "IN_PROGRESS" && jsonData.request.intent.slots.cityName.confirmationStatus == "DENIED") {
-              var city2 = jsonData.request.intent.slots.cityName.value;
-              responseBody = {
-                "version": "1.0",
-                "response": {
-                "directives": [
-                  {
-                    "type": "Dialog.Delegate",
-                    "updatedIntent": {
-                      "name": "cityIntent",
-                      "confirmationStatus": "NONE",
-                      "slots": {
-                        "cityName": {
-                          "name": "cityName",
-                          "value": city2,
-                          "confirmationStatus": "DENIED"
+                    "outputSpeech": {
+                        "type": "PlainText",
+                        "text": "Welcome to Shubham's Stormy weather assistant! Please say a command."
+                    },
+                    "card": {
+                        "type": "Simple",
+                        "title": "Opened",
+                        "content": "You started the Node.js Echo API Sample"
+                    },
+                    "reprompt": {
+                        "outputSpeech": {
+                            "type": "PlainText",
+                            "text": "Say a command"
                         }
-                      }
-                    }
-                  }
-                ],
-                "shouldEndSession": false
+                    },
+                    "shouldEndSession": false
                 }
             };
-            } else if (jsonData.request.dialogState == "IN_PROGRESS") {
-              let response = deasync(function(callback){
-                weather.cityWeather(city, callback);
-              })();
-
-              var outputSpeechText = "humidity is " + response.body.main.humidity + " with " + response.body.weather[0].description + ".";
-              responseBody = {
-                "version": '1.0',
-                "response": {
-                    "shouldEndSession": true,
-                    "outputSpeech": { "type": 'SSML', "ssml": '<speak>' + outputSpeechText + '</speak>' } 
-                },
-                "sessionAttributes": {},
-                "userAgent": 'ask-nodejs/1.0.25 Node/v6.10.0'
-            }
-            }
-      } else {
-        responseBody = {
-          "version": "1.0",
-          "response": {
-          "directives": [
-            {
-              "type": "Dialog.Delegate",
-              "updatedIntent": {
-                "name": "cityIntent",
-                "confirmationStatus": "NONE",
-                "slots": {
-                  "cityName": {
-                    "name": "cityName",
-                    "confirmationStatus": "NONE"
-                  }
+        } else if(jsonData.request.type == "IntentRequest") {
+            var outputSpeechText = "";
+            var cardContent = "";
+            if (jsonData.request.intent.name == "WeatherIntent") {
+                //response modified
+                // The Intent "TurnOn" was successfully called
+                responseBody = {
+                    "version": '1.0',
+                    "response": {
+                        "shouldEndSession": true,
+                        "outputSpeech": { "type": 'SSML', "ssml": '<speak> Hello from node JS </speak>' } 
+                    },
+                    "sessionAttributes": {},
+                    "userAgent": 'ask-nodejs/1.0.25 Node/v6.10.0'
                 }
-              }
+            } else if (jsonData.request.intent.name == "cityIntent") {
+                if (typeof jsonData.request.intent.slots.cityName.value != "undefined") {
+                    var city = jsonData.request.intent.slots.cityName.value;
+                    if (jsonData.request.dialogState == "STARTED") {
+                        //var outputSpeechText = "humidity is " + response.body.main.humidity + " with " + response.body.weather[0].description + ".";
+                        responseBody = {
+                            "version": "1.0",
+                            "response": {
+                                "directives": [
+                                    {
+                                        "type": "Dialog.Delegate",
+                                        "updatedIntent": {
+                                            "name": "cityIntent",
+                                            "confirmationStatus": "NONE",
+                                            "slots": {
+                                                "cityName": {
+                                                    "name": "cityName",
+                                                    "value": city,
+                                                    "confirmationStatus": "NONE"
+                                                }
+                                            }
+                                        }
+                                    }
+                                ],
+                                "shouldEndSession": false
+                            }
+                        };
+                    } else if (jsonData.request.dialogState == "IN_PROGRESS" && jsonData.request.intent.slots.cityName.confirmationStatus == "CONFIRMED") {
+                        let response = deasync(function(callback){
+                            weather.cityWeather(city, callback);
+                        })();
+
+                        var outputSpeechText = "humidity is " + response.body.main.humidity + " with " + response.body.weather[0].description + ".";
+                        responseBody = {
+                            "version": '1.0',
+                            "response": {
+                                "shouldEndSession": true,
+                                "outputSpeech": { "type": 'SSML', "ssml": '<speak>' + outputSpeechText + '</speak>' } 
+                            },
+                            "sessionAttributes": {},
+                            "userAgent": 'ask-nodejs/1.0.25 Node/v6.10.0'
+                        }
+                    } else if (jsonData.request.dialogState == "IN_PROGRESS" && jsonData.request.intent.slots.cityName.confirmationStatus == "DENIED") {
+                        var city2 = jsonData.request.intent.slots.cityName.value;
+                        responseBody = {
+                            "version": "1.0",
+                            "response": {
+                                "directives": [
+                                    {
+                                        "type": "Dialog.Delegate",
+                                        "updatedIntent": {
+                                            "name": "cityIntent",
+                                            "confirmationStatus": "NONE",
+                                            "slots": {
+                                                "cityName": {
+                                                    "name": "cityName",
+                                                    "value": city2,
+                                                    "confirmationStatus": "DENIED"
+                                                }
+                                            }
+                                        }
+                                    }
+                                ],
+                                "shouldEndSession": false
+                            }
+                        };
+                    } else if (jsonData.request.dialogState == "IN_PROGRESS") {
+                        let response = deasync(function(callback){
+                            weather.cityWeather(city, callback);
+                        })();
+
+                        var outputSpeechText = "humidity is " + response.body.main.humidity + " with " + response.body.weather[0].description + ".";
+                        responseBody = {
+                            "version": '1.0',
+                            "response": {
+                                "shouldEndSession": true,
+                                "outputSpeech": { "type": 'SSML', "ssml": '<speak>' + outputSpeechText + '</speak>' } 
+                            },
+                            "sessionAttributes": {},
+                            "userAgent": 'ask-nodejs/1.0.25 Node/v6.10.0'
+                        };
+                    }
+                } else {
+                    responseBody = {
+                        "version": "1.0",
+                        "response": {
+                        "directives": [
+                            {
+                                "type": "Dialog.Delegate",
+                                "updatedIntent": {
+                                    "name": "cityIntent",
+                                    "confirmationStatus": "NONE",
+                                    "slots": {
+                                        "cityName": {
+                                            "name": "cityName",
+                                            "confirmationStatus": "NONE"
+                                        }
+                                    }
+                                }
+                            }
+                        ],
+                        "shouldEndSession": false
+                        }
+                    };
+                }
             }
-          ],
-          "shouldEndSession": false
-          }
-      };
-      }
-    }
-      }
-    res.statusCode = 200;
-    res.contentType('application/json');
-    res.send(responseBody);
+        }
+        res.statusCode = 200;
+        res.contentType('application/json');
+        res.send(responseBody);
     });
-  });
+});
 
 app.listen(port);
