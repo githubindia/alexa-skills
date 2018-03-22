3c117325cfa3a23560c9ebd27cc42e3c1d420a93
Shubham Gupta <shubham08gupta08@gmail.com>
2018-03-22 12:45:55 +0530

    weather api key changed

--- src/weatherApi.js
+++ src/weatherApi.js
@@ -2,14 +2,14 @@ var request = require("request");
  
 module.exports = {
     'cityWeather' : function(city, callback){
- 
+        //http://api.openweathermap.org/data/2.5/weather?q=chennai&appid=4d81047a5d639a9baf2ca66e875c0069
         //console.log("The Final Message Utterance to send POST as Query to Service Now");
         var options = { 
             method: 'GET',
-            url: 'http://samples.openweathermap.org/data/2.5/weather',
+            url: 'http://api.openweathermap.org/data/2.5/weather',
             qs: { 
                 "q":city,
-                "appid":'b6907d289e10d714a6e88b30761fae22'                    
+                "appid":'4d81047a5d639a9baf2ca66e875c0069'                    
             },
             json:true
         };
